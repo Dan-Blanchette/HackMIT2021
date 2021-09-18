8f1d730cdc6ddfd8f19cdd538e671ed04d1c9d51
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-09-18 16:46:40 -0700

    final version of earthquake data

--- readable_eq_data.py
+++ readable_eq_data.py
@@ -4,23 +4,37 @@ from plotly.graph_objects import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'significant.json'
+filename = 'data/significant.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
 
-mags, longi, lats, = [] ,[], []
+mags, longi, lats, hover_texts = [] ,[], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     longi.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 # map the earthquakes
-data = [Scattergeo(lon=longi, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': longi, 
+    'lat': lats,
+    'text': hover_texts,
+    'marker': { 
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    },
+}]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data' : data, 'layout' : my_layout}
